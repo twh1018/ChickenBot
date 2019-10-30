fefbaea0d0e89c93ba85bb799d41212a7bf96df8
twh1018 <57197665+twh1018@users.noreply.github.com>
2019-10-30 17:11:32 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,74 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = '!'
+
+
+
+const boredMessages = [
+    'Go Play Some Minecraft',
+    'Homework? Nah...',
+    '<:emoji_2:636776257853587457>CLASH ROYALE<:emoji_2:636776257853587457>',
+    'Stay And Chat A While',
+    'Go Outside And Enjoy The Fresh Air',
+    'Eat More Cowz',
+    'Do Some Stuff...',
+    'Reflect On Why You Are Telling A Robot You Are Bored'
+    ]
+
+
+
+bot.on('ready', () =>{
+    console.log('I\'m Online')
+})
+bot.on('message',message=>{
+    
+
+    msg = message.content.toLowerCase()
+
+    
+    if(message.author.bot){
+        return
+    }
+
+
+  
+        if(msg.startsWith(prefix+'help')){
+            message.channel.send('Commands:')
+            message.channel.send('!hi')
+            message.channel.send('!no')
+            message.channel.send('!I\'m Bored')
+        }
+
+
+
+
+        if(msg.startsWith(prefix+'no')){
+            message.reply('Yes!!!')
+        }
+
+ 
+
+        if(msg.startsWith(prefix+'hi')){
+            message.channel.send('Hello '+message.author)
+        }
+
+
+
+       if(msg.startsWith(prefix + "i'm bored")){
+            var randMessageNum =Math.floor(Math.random()*8)
+            const boredMessages = [
+                'Go Play Some Minecraft',
+                'Homework? Nah...',
+                '<:emoji_2:636776257853587457>CLASH ROYALE<:emoji_2:636776257853587457>',
+                'Stay And Chat A While',
+                'Go Outside And Enjoy The Fresh Air',
+                'Eat More Cowz',
+                'Do Some Stuff...',
+                'Why Are You Telling A Robot You Are Bored?'
+            ]
+            message.reply(boredMessages[randMessageNum])
+            }
+        
+})
+
+bot.login(process.env.BotToken);
